828c14cef89f33241fbe3a67c840bdd8938e8656
FoxOnefr <foxonefr@gmail.com>
2025-03-14 14:25:18 +0100

    sql

---
+++ sql/feed_base.sql
@@ -0,0 +1,8 @@
+USE meme_generator;
+
+INSERT INTO memes (image, text, votes) VALUES
+('https://static.independent.co.uk/s3fs-public/thumbnails/image/2016/02/01/13/jameson-pfeiffer-syndrome.jpg', "J'ai arrêté un bus avec ma tête", 0),
+('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTNIvCHLJlLdfWjZ6M7jo4zxNf81I-PtTCf5g&s', 'Moi après 3 cafés ☕☕☕',0),
+('https://i.pinimg.com/474x/35/25/3f/35253f64facc999e781f9d679feb1841.jpg', 'huh', 0),
+('https://us-tuna-sounds-images.voicemod.net/a594cc56-2b00-4202-90d0-0de460861df5-1663985224805.png', 'Amogus', 0),
+('https://images.steamusercontent.com/ugc/2044108594145854221/7591EFF18A70AA34DBC34839D1AABF111318C709/', 'HIN HIN', 0);
