5df59502a737cd0d424ab551f9dd6aadbfddf7e1
Hugo2905 <hugo.cognard2005@gmail.com>
2025-04-17 08:20:27 +0200

    Apache test

--- include/connect.php
+++ include/connect.php
@@ -1,7 +1,9 @@
-<?php
+<?php	
+		define ('SERVEUR_BD', getenv('SERVEUR_BD'));
 	    define ('HOST','c.mariadb:3306');
 	    define ('MARIADB_USER','route');
 	    define ('MARIADB_PASSWORD','root');
 	    define ('MARIADB_DATABASE','meme-generator'); 
 		define( 'MARIADB_ROOT_PASSWORD','qh');
+		
 
